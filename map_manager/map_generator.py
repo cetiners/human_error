71fdc2a906b75142ead81ff21b553fd46c8453c3
cetiners <70922017+cetiners@users.noreply.github.com>
2022-04-21 16:35:17 +0200

    minor improvements

--- map_manager/map_generator.py
+++ map_manager/map_generator.py
@@ -10,6 +10,8 @@ class map():
     def __init__(self, name="Map", size=1024):
         self.name = name
         self.size = int(size**0.5)
+
+        # Initialize map with an array of zeros.
         self.coord = np.zeros((self.size,self.size),dtype="int")
 
         self.location_coordinates = {}
@@ -20,23 +22,30 @@ class map():
          
 
     def place_character(self):
+        # Return random a random coordinate
         return [random.uniform(0.1,float(self.size)) for i in range(2)]
 
-    def populate_map(self, n_locations=10, name="Regions", is_event=False):
+    def populate_map(self, n_locations=10, name="Regions", is_event=False, print=False):
+
+        # Place centroids to be used as region centres.
         centroids = [tuple([random.uniform(0.1,float(self.size)) for i in range(2)]) for i in range(n_locations)]
         centroids_rounded = [[float(format(centroid[0], '.2f')),float(format(centroid[1], '.2f'))] for centroid in centroids]
+
+        # Generate Voronoi regions (All points closer to one specific centroid is in a region.)
         vor = Voronoi(centroids_rounded)
-        fig = voronoi_plot_2d(vor, show_vertices=False, line_colors='black',line_width=0.5, line_alpha=0.6, point_size=1)
-        fig.set_size_inches(18.5, 10.5)
-        for region in vor.regions:
-            if not -1 in region:
-                polygon = [vor.vertices[i] for i in region]
-                plt.fill(*zip(*polygon),alpha=0.4)
-                plt.xlim(right=0, left=self.size)
-                plt.ylim(bottom=0, top=self.size)
+        if print:
+            fig = voronoi_plot_2d(vor, show_vertices=False, line_colors='black',line_width=0.5, line_alpha=0.6, point_size=1)
+            fig.set_size_inches(18.5, 10.5)
+            for region in vor.regions:
+                if not -1 in region:
+                    polygon = [vor.vertices[i] for i in region]
+                    plt.fill(*zip(*polygon),alpha=0.4)
+                    plt.xlim(right=0, left=self.size)
+                    plt.ylim(bottom=0, top=self.size)
         self.vors[name] = vor
         self.location_coordinates[name] = vor.points
         self.location_events[name] = is_event
+        
         return vor
 
     def event_starter(self, event, player):
@@ -48,23 +57,33 @@ class map():
 
 
     def check_region(self,coordinates):
+        
         points = self.vor.points
         voronoi_kdtree = cKDTree(points)
         dst, regions = voronoi_kdtree.query(coordinates)
+        
         return regions
 
-    def create_regions(self, name, potential_regions, region_weights):
+    def create_regions(self, name, potential_regions,region_weights=[]):
+
+        if potential_regions == []:
+        
+            region_weights = [(1/len(potential_regions)) for i in range(len(potential_regions))]
+        
         types = {}
+        
         for i in range(0,len(self.vors[name].regions)):    
             types[str(self.vors[name].regions[i])] = (random.choices(list(potential_regions.keys()), weights =region_weights,k=1))[0]
 
         fig = voronoi_plot_2d(self.vors[name], show_vertices=False, line_colors='white',line_width=0.0, line_alpha=0.0, point_size=1)
         fig.set_size_inches(18.5, 10.5)
+        
         for region in self.vors[name].regions:
             if not -1 in region:
                 polygon = [self.vors[name].vertices[i] for i in region]
                 plt.fill(*zip(*polygon),color=potential_regions[types[str(region)]],alpha=0.4)
             plt.xlim(right=0, left=self.size)
             plt.ylim(bottom=0, top=self.size)
+        
         return types
         
--- tools/flavour.py
+++ tools/flavour.py
@@ -12,16 +12,16 @@ def typing(message):
     print("")
     #print(message) # Eliminate this after testing...
     for word in message:
-        time.sleep(random.choice([0.3, 0.11, 0.08, 0.07,   0.07, 0.07, 0.06, 0.06, 0.05, 0.01]))
+        time.sleep(random.choice([0.3, 0.11, 0.08, 0.07, 0.07, 0.07, 0.06, 0.06, 0.05, 0.01]))
         sys.stdout.write(word)
         sys.stdout.flush()
     time.sleep(.1)
 
-for i in range(2):
+for i in range(1):
     typing("...")
-    time.sleep(0.5)
+    time.sleep(0.1)
     os.system("clear")
 typing("Cold")
-time.sleep(1)
+
 os.system("clear")
 typing("It is very cold")
