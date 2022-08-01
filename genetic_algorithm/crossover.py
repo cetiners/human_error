49492cf1cc6767a50d67062fd8ff430143f7ebe0
cetiners <70922017+cetiners@users.noreply.github.com>
2022-08-01 16:00:57 +0300

    xo_addition

--- genetic_algorithm/crossover.py
+++ genetic_algorithm/crossover.py
@@ -1,4 +1,6 @@
 
+from map_manager.world_encounters import *
+
 
 def coordinate_xo(parent_1, parent_2):
 
@@ -15,4 +17,29 @@ def partially_mapped_xo(parent_1,parent_2):
     """
     Takes a "pack" of world encounters, 
     """
-    pass
+    parent_1_coor = [i.coord for i in parent_1.pack] 
+    parent_2_coor = [i.coord for i in parent_2.pack]
+
+    offspring_1 = parent_1.copy()
+    offspring_2 = parent_2.copy()
+    
+    #offspring_1 = pack(parent_1.pack[0].map, type=parent_1.pack[0].type ,size=parent_1.pack[0].size, encounter_type=parent_1.pack[0].encounter_type)
+    #offspring_2 = pack(parent_1.pack[0].map, type=parent_1.pack[0].type ,size=parent_1.pack[0].size, encounter_type=parent_1.pack[0].encounter_type)
+
+    "XO Schenenagens"
+
+    cut_points = sorted([random.randint(0,len(parent_1_coor)) for i in range(2)])
+
+
+    new_coord_1 = parent_1_coor.copy()
+    new_coord_2 = parent_2_coor.copy()
+
+    for i in range(cut_points[0],cut_points[1]+1):
+    
+        new_coord_1[i] = parent_2_coor[i]
+        new_coord_2[i] = parent_1_coor[i]
+
+    for i in range(offspring_1.pack):
+        offspring_1[i].coord = new_coord_1[i]
+
+    return offspring_1, offspring_2
